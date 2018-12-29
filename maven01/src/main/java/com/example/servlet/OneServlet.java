67c49d7cc43a6827a3951c5e3c6b56a215d1e792
Mrslian <1395124699@qq.com>
2018-12-29 16:13:25 +0800

    基本项目

---
+++ maven01/src/main/java/com/example/servlet/OneServlet.java
@@ -0,0 +1,26 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OneServlet extends HttpServlet {
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		response.getWriter().write("hello git");
+	
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
